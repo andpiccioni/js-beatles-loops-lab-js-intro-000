2fac95c0c1f816a52c937ef27bcd664b0f8dde36
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-12 10:25:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function johnLennonFacts(facts) {
   ];
   var i = 0;
   while (i < facts.length){
-    facts[i] = `${facts[i]} !!!`;
+    facts[i] = `${facts[i]}!!!`;
     i++;
   }
   return facts
