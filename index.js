c5efaf98b9cc795f1e8f954e2d546b49b70b0c47
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-12 09:29:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,31 @@
+var musician = ["John Lennon", "Paul McCartney", "George Morrison", "Ringo Starr"]
+var instrument = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
+
+function theBeatlesPlay(musician, instrument){
+  var beatArray = []
+  for(var i = 0; i < musician.length; i++){
+    beatArray.push(`${musician[i]} plays ${instrument[i]}`)
+  }
+  return beatArray
+}
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+
+
+
+
+function iLoveTheBeatles(n){
+  var emptArray = []
+  var n = 0
+  do {
+    emptArray.push("I love the Beatles!");
+    n++;
+  }
+  while (n < 15)
+}
