9a7809056f4bcd94b61fa17741e0028939b264c4
Andrea Piccioni <andpiccioni@gmail.com>
2017-10-12 10:02:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,3 +22,20 @@ function iLoveTheBeatles(i){
   while (i < 15);
   return emptArray;
 }
+
+
+const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+
+var i = 0;
+
+function johnLennonFacts(facts) {
+  while (i < facts.length + 1){
+    facts[i] = `"${facts[i]} !!!"`
+  }
+  return facts
+}
